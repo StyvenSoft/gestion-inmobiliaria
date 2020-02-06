d18a2038d081c76fe7059aff567cc01a59085e38
Steveen <steveen.echeverri@gmail.com>
2020-02-06 18:49:31 -0500

    Add buttons in bar session

--- src/components/layout/bar/BarSession.js
+++ src/components/layout/bar/BarSession.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Toolbar, Typography, Button } from '@material-ui/core';
+import { Toolbar, Typography, Button, IconButton } from '@material-ui/core';
 import { withStyles } from '@material-ui/styles';
 
 const styles = theme =>({
@@ -8,7 +8,16 @@ const styles = theme =>({
         [theme.breakpoints.up("md")] : {
             display : "flex"
         }
-    } 
+    },
+    sectionMobile : {
+        display : "flex",
+        [theme.breakpoints.up("md")] : {
+            display : "none"
+        }
+    },
+    grow : {
+        flexGrow : 1
+    }
 });
 
 class BarSession extends Component {
@@ -19,9 +28,18 @@ class BarSession extends Component {
         return (
             <div>
                 <Toolbar>
+                <IconButton color="inherit">
+                            <i className="material-icons">menu</i>
+                        </IconButton>
                     <Typography variant="h6">Inmobiliaria Seveen</Typography>
+                    <div className={classes.grow}></div>
                     <div className={classes.sectionDesktop}>
-                        <Button>Login</Button>
+                        <Button color="inherit">Login</Button>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton color="inherit">
+                            <i className="material-icons">more_vert</i>
+                        </IconButton>
                     </div>
                 </Toolbar>
             </div>
