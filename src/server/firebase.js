241202e8a27f00d48763b099553bd812ab0c93c6
Steveen <steveen.echeverri@gmail.com>
2020-02-08 22:17:33 -0500

    Instance and config init firebase app

---
+++ src/server/firebase.js
@@ -0,0 +1,21 @@
+import app from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCtV64E49IuGVwTVYdXwjOgZTZt-fucyng",
+    authDomain: "gestion-inmoviliaria.firebaseapp.com",
+    databaseURL: "https://gestion-inmoviliaria.firebaseio.com",
+    projectId: "gestion-inmoviliaria",
+    storageBucket: "gestion-inmoviliaria.appspot.com",
+    messagingSenderId: "368590167279",
+    appId: "1:368590167279:web:c5e9fe43d4448143eccf1a",
+    measurementId: "G-X01V32VNTN"
+};
+
+class Firebase {
+    constructor(){
+        app.initializeApp(config);
+        this.db = app.firestore();
+    }
+}
+
+export default Firebase;
