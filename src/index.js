244a7fe39bb3c344d7c6545a3189f71a5adedbb6
Steveen <steveen.echeverri@gmail.com>
2020-02-13 11:44:56 -0500

    Session setup finished

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './server';
-
+import { initialState } from './session/initialState';
+import { StateProvider } from './session/store';
+import sessionReducer from './session/reducers/sessionReducer';
 //const FirebaseContext = React.createContext();
 
 ReactDOM.render(
 <FirebaseContext.Provider value={new Firebase()}>
-    <App />
+    <StateProvider initialState={initialState} reducer={sessionReducer}>
+        <App />
+    </StateProvider>
 </FirebaseContext.Provider>
 , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
