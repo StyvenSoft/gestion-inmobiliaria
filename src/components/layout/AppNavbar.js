5027266be2145c219992e620082724694f620fbf
Steveen <steveen.echeverri@gmail.com>
2020-02-21 18:24:44 -0500

    Create Componet Did Mount in app Navbar

--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
 import  AppBar  from "@material-ui/core/AppBar";
 import BarSession from './bar/BarSession';
+import  { withStyles }  from "@material-ui/styles";
+import { compose } from 'recompose';
+import { consumerFirebase } from '../../server';
+import { StateContext } from '../../session/store'
 
-export default class AppNavbar extends Component {
+const styles = theme => ({
+    sectionDesktop : {
+        display : "none",
+        [theme.breakpoints.up("md")] : {
+            display : "flex"
+        }
+    }, 
+    sectionMobile : {
+        display : "flex",
+        [theme.breakpoints.up("md")] : {
+            display : "none"
+        }
+    }
+})
+
+class AppNavbar extends Component {
+
+    static contextType = StateContext;
+
+    state = {
+        firebase : null
+    }
+
+    componentDidMount() {
+        const { firebase } = this.state;
+        const [{session}, dispatch] = this.context;
+
+        if (firebase.auth.currentUser !== null && !session) {
+            firebase.db
+                .collection("Users")
+                .doc(firebase.auth.currentUser.uid)
+                .get()
+                .then(doc => {
+                    const userDB = doc.data();
+                    dispatch({
+                        type : "LOGIN",
+                        session : userDB,
+                        authenticated : true
+                    })
+                })
+        }
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        let newObjects = {};
+        if (nextProps.firebase !== prevState.firebase) {
+            newObjects.firebase = nextProps.firebase
+        } else {
+            return newObjects;
+        }
+    }
+
     render() {
-        return (
+
+        const [{session}, dispatch] = this.context;
+        
+        return session ? (session.authenticated ? (
             <div>
                 <AppBar position="static">
                     <BarSession />
                 </AppBar>
             </div>
-        )
+            ) : null 
+        ) : null;
     }
 }
+
+export default compose(withStyles(styles), consumerFirebase)(AppNavbar);
