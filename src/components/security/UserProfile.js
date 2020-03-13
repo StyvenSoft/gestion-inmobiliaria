f520b4cf4c45abbca7a521ffc708a1a5209b9188
Steveen <steveen.echeverri@gmail.com>
2020-03-12 19:56:47 -0500

    add logic hook component UserProfile

--- src/components/security/UserProfile.js
+++ src/components/security/UserProfile.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useStateValue } from '../../session/store'
+import { useStateValue } from '../../session/store';
 import { Container, Avatar, Typography, Grid, TextField, Button } from '@material-ui/core';
-import reactPhoto from '../../user.png'
+import reactPhoto from '../../user.png';
+import {consumerFirebase} from '../../server';
+import { openScreenMessage } from '../../session/actions/snackbarAction';
 
 const style = {
     paper : {
@@ -26,6 +28,7 @@ const style = {
 
 const UserProfile = props => {
     const [{session}, dispatch] = useStateValue();
+    const firebase = props.firebase;
 
     let [state, changeState] = useState({
         name : "",
@@ -34,6 +37,48 @@ const UserProfile = props => {
         phone : "",
         id : "",
         photo : ""
+    });
+
+    const changeData = e => {
+        const {name, value} = e.target;
+        changeState(prev => ({
+            ...prev,
+            [name] : value
+        }))
+    }
+
+    const saveChange = e => {
+        e.preventDefault();
+
+        firebase.db
+        .collection('Users')
+        .doc(firebase.auth.currentUser.uid)
+        .set(state, {merge: true})
+        .then(success => {
+            dispatch({
+                type : 'LOGIN',
+                session : state,
+                authenticated : true
+            })
+            openScreenMessage(dispatch, {
+                open : true,
+                messages : 'Se guardaron los cambios'
+            })
+        })
+        .catch(error => {
+            openScreenMessage(dispatch, {
+                open : true,
+                messages : 'Error guardando en la base de datos' + error
+            })
+        })
+    }
+
+    useEffect(() => {
+        if(state.id === ''){
+            if(session) {
+                changeState(session.user);
+            }
+        }
     })
 
     return (session
@@ -45,21 +90,21 @@ const UserProfile = props => {
                         <form style={style.form}>
                             <Grid container spacing={2}>
                                 <Grid item xs={12} md={6}>
-                                    <TextField name="name" variant="outlined" fullWidth label="Nombre" />
+                                    <TextField name="name" variant="outlined" fullWidth label="Nombre" value={state.name} onChange={changeData} />
                                 </Grid>
                                 <Grid item xs={12} md={6}>
-                                    <TextField name="lastname" variant="outlined" fullWidth label="Apellido" />
+                                    <TextField name="lastname" variant="outlined" fullWidth label="Apellido" value={state.lastname} onChange={changeData} />
                                 </Grid>
                                 <Grid item xs={12} md={6}>
-                                    <TextField name="email" variant="outlined" fullWidth label="Email" />
+                                    <TextField name="email" variant="outlined" fullWidth label="Email" value={state.email} onChange={changeData} />
                                 </Grid>
                                 <Grid item xs={12} md={6}>
-                                    <TextField name="phone" variant="outlined" fullWidth label="Teléfono" />
+                                    <TextField name="phone" variant="outlined" fullWidth label="Teléfono" value={state.phone} onChange={changeData} />
                                 </Grid>
                             </Grid>
                             <Grid container justify="center">
                                 <Grid item xs={12} md={6}>
-                                    <Button type="submit" fullWidth variant="contained" size="large" color="primary" style={style.submit}>
+                                    <Button type="submit" fullWidth variant="contained" size="large" color="primary" style={style.submit} onClick={saveChange} >
                                         Guardar cambios
                                     </Button>
                                 </Grid>
@@ -72,4 +117,4 @@ const UserProfile = props => {
         );
 }
 
-export default UserProfile;
+export default consumerFirebase(UserProfile);
