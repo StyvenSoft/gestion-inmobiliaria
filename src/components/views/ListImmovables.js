4bc363c7bca48663c341379e518354fe45652648
Steveen <steveen.echeverri@gmail.com>
2020-08-17 23:06:21 -0500

    Created property list component

--- src/components/views/ListImmovables.js
+++ src/components/views/ListImmovables.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import { Container, Paper, Grid, Breadcrumbs, Link, Typography, TextField } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import { consumerFirebase } from '../../server';
 
-export default class ListImmovables extends Component {
+const style = {
+    cardGrid: {
+        paddingTop: 8,
+        paddingBotton: 8
+    },
+    paper: {
+        backgroundColor: "#f5f5f5",
+        paddind: "20px",
+        height: 650
+    },
+    link: {
+        display: "flex"
+    },
+    gridText: {
+        marginTop: "20px"
+    }
+}
+
+class ListImmovables extends Component {
     render() {
         return (
-            <div>
-                <Button variant="contained" color="primary">Color primario</Button>
-                <Button variant="contained" color="secondary">Color secundario</Button>
-            </div>
+            <Container style={style.cardGrid}>
+                <Paper style={style.paper}>
+                    <Grid item xs={12} sm={12}>
+                        <Breadcrumbs arial-label="breadcrums">
+                            <Link color="inherit" style={style.link} href="/">
+                                <HomeIcon />
+                                Home
+                            </Link>
+                            <Typography color="textPrimary">Mis Inmuebles</Typography>
+                        </Breadcrumbs>
+                    </Grid>
+                    <Grid item xs={12} sm={6} style={style.gridText}>
+                        <TextField
+                            fullWidth
+                            ImputsLabelProps={{
+                                shrink: true
+                            }}
+                            name="searchText"
+                            variant="outlined"
+                            label="Ingrese el inmueble a buscar"
+                        ></TextField>
+                    </Grid>
+                </Paper>
+            </Container>
         )
     }
 }
+
+export default consumerFirebase(ListImmovables);
