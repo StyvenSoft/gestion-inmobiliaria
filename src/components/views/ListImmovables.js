1e8a3b49b8b65900b8036acd3764870470f128ef
Steveen <steveen.echeverri@gmail.com>
2020-09-28 16:13:13 -0500

    Create grid for property list

--- src/components/views/ListImmovables.js
+++ src/components/views/ListImmovables.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Paper, Grid, Breadcrumbs, Link, Typography, TextField } from '@material-ui/core';
+import { Container, Paper, Grid, Breadcrumbs, Link, Typography, TextField, Card, CardMedia, CardContent, CardActions, Button } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import { consumerFirebase } from '../../server';
+import logo from '../../home.png';
 
 const style = {
     cardGrid: {
@@ -11,13 +12,31 @@ const style = {
     paper: {
         backgroundColor: "#f5f5f5",
         paddind: "20px",
-        height: 650
+        height: 650,
+        padding: '20px'
     },
     link: {
         display: "flex"
     },
+    homeIcon: {
+        width: 20,
+        height: 20,
+        marginRight: '4px'
+    },
     gridText: {
-        marginTop: "20px"
+        marginTop: "20px",
+        marginLeft: "15px"
+    },
+    card: {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    cardMedia: {
+        paddingTop: "56.25%",
+    },
+    cardContent: {
+        flexGrow: 1
     }
 }
 
@@ -40,7 +59,7 @@ class ListImmovables extends Component {
         const arrayInmueble = snapshot.docs.map(doc => {
             let data = doc.data();
             let id = doc.id;
-            return {id, ...data};
+            return { id, ...data };
         })
 
         this.setState({
@@ -52,10 +71,10 @@ class ListImmovables extends Component {
         return (
             <Container style={style.cardGrid}>
                 <Paper style={style.paper}>
-                    <Grid item xs={12} sm={12}>
+                    <Grid item xs={12} sm={8}>
                         <Breadcrumbs arial-label="breadcrums">
                             <Link color="inherit" style={style.link} href="/">
-                                <HomeIcon />
+                                <HomeIcon style={style.homeIcon} />
                                 Home
                             </Link>
                             <Typography color="textPrimary">Mis Inmuebles</Typography>
@@ -74,13 +93,44 @@ class ListImmovables extends Component {
                             value={this.state.searchText}
                         ></TextField>
                     </Grid>
-                    <Grid item xs={12} sm={12}>
+                    <Grid item xs={12} sm={12} style={style.gridText}>
                         <Grid container spacing={4}>
-                            {this.state.inmuebles.map(card => {
-                                // <Grid item key={card.id} xs={12} sm={6} md={4}>
-                                    
-                                // </Grid>
-                            })}
+                            {this.state.inmuebles.map(card => (
+                                <Grid item key={card.id} xs={12} sm={6} md={4}>
+                                    <Card style={style.card}>
+                                        <CardMedia
+                                            style={style.cardMedia}
+                                            image={
+                                                card.photos
+                                                    ? card.photos[0]
+                                                        ? card.photos[0]
+                                                        : logo
+                                                    : logo
+                                            }
+                                        />
+                                        <CardContent style={card.content}>
+                                            <Typography gutterBotton variand="h5" component="h2">
+                                                {card.city + ", " + card.country}
+                                            </Typography>
+                                        </CardContent>
+
+                                        <CardActions>
+                                            <Button 
+                                                size="small"
+                                                color="primary"
+                                            >
+                                                Editar
+                                            </Button>
+                                            <Button 
+                                                size="small"
+                                                color="primary"
+                                            >
+                                                Eliminar
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
                         </Grid>
                     </Grid>
                 </Paper>
