403bfaa79a5fa2901abf65aa9fe016e0e9112427
Steveen <steveen.echeverri@gmail.com>
2020-12-31 12:42:11 -0500

    Send emails from the Hook

--- src/components/views/ListUsers.js
+++ src/components/views/ListUsers.js
@@ -1,7 +1,10 @@
 import { Button, Container, Grid, Paper, Table, TableBody, TableCell, TableRow } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { sendEmail } from '../../redux/actions/emailAction';
 import { getUsersApp } from '../../redux/actions/userAction';
+import { useStateValue } from "../../../session/store";
+import { openScreenMessage } from '../../session/actions/snackbarAction';
 
 const style = {
     paper: {
@@ -19,17 +22,32 @@ const style = {
 
 const ListUsers = (props) => {
 
+    const [{ session }, dispatch] = useStateValue();
     const [isLoading, setIsLoading] = useState(false);
     const listArray = useSelector(state => state.userRedux.users);
-    const dispatch = useDispatch();
+    const dispatchRedux = useDispatch();
 
     useEffect(() => {
         if (!isLoading) {
-            getUsersApp(dispatch).then(success => {
+            getUsersApp(dispatchRedux).then(success => {
                 setIsLoading(true);
             })
         }
     })
+
+    const buttonSendEmail = (email) => {
+        const obj = {
+            email: email,
+            title: "Mensaje enviado desde la App Inmobiliaria",
+            message: "Gracias por enviar el mensaje..."
+        }
+        sendEmail(obj).then(data => {
+            openScreenMessage(dispatch, {
+                open: true,
+                message: "Se envió en correo electrónico al destinatario" + email,
+            })
+        })
+    };
 
     return (
         <Container style={style.container}>
@@ -46,10 +64,19 @@ const ListUsers = (props) => {
                                                 {row.name ? row.name + " " + row.lastname : ""}
                                             </TableCell>
                                             <TableCell>
-                                                <Button variant="contained" color="primary" size="small">Roles</Button>
+                                                <Button 
+                                                    variant="contained" 
+                                                    color="primary" 
+                                                    size="small"
+                                                >Roles</Button>
                                             </TableCell>
                                             <TableCell>
-                                                <Button variant="contained" color="primary" size="small">Enviar Mensaje</Button>
+                                                <Button 
+                                                    variant="contained" 
+                                                    color="primary" 
+                                                    size="small"
+                                                    onClick={() => buttonSendEmail(row.email)}
+                                                >Enviar Mensaje</Button>
                                             </TableCell>
                                         </TableRow>
                                             )
